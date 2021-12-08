67ca277f5f422951bbf2cb4a8bd9d1625a934b8d
양태웅 <didxodnd0422@gmail.com>
2021-12-08 17:39:08 +0900

    Initialize Project

---
+++ app/src/main/java/com/example/mealgo/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mealgo.ui
+
+import com.example.mealgo.R
+import com.example.mealgo.databinding.ActivityMainBinding
+import com.example.mealgo.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun init() {
+
+    }
+}
