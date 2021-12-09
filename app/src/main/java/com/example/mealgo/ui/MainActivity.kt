ec7d3afcbfa0b9436d05c5e588ed74cca5761344
양태웅 <didxodnd0422@gmail.com>
2021-12-09 18:16:16 +0900

    Connect NavHostFragment with BottomNavigationView

--- app/src/main/java/com/example/mealgo/ui/MainActivity.kt
+++ app/src/main/java/com/example/mealgo/ui/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.mealgo.ui
 
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.mealgo.R
 import com.example.mealgo.databinding.ActivityMainBinding
 import com.example.mealgo.base.BaseActivity
@@ -7,7 +12,22 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
-    override fun init() {
+    private lateinit var navController: NavController
 
+    override fun init() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.schoolFragment,
+                R.id.profileFragment
+            )
+        )
+
+        binding.bottomNav.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onNavigateUp(): Boolean = navController.navigateUp() || super.onNavigateUp()
 }
