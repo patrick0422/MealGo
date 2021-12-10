38b6b925259980557a49b3e5fd3ae1aa69351947
양태웅 <didxodnd0422@gmail.com>
2021-12-10 15:43:55 +0900

    Add MealFragment

---
+++ app/src/main/java/com/example/mealgo/ui/meal/MealFragment.kt
@@ -0,0 +1,14 @@
+package com.example.mealgo.ui.meal
+
+import com.example.mealgo.R
+import com.example.mealgo.base.BaseFragment
+import com.example.mealgo.databinding.FragmentMealBinding
+
+
+class MealFragment : BaseFragment<FragmentMealBinding>(R.layout.fragment_meal) {
+
+
+    override fun init() {
+
+    }
+}
