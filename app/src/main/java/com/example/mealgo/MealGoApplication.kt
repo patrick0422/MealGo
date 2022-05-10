09bce336739bad923887be849bfea824e154cb98
양태웅 <didxodnd0422@gmail.com>
2022-05-10 21:09:06 +0900

    Apply misc changes

--- app/src/main/java/com/example/mealgo/MealGoApplication.kt
+++ app/src/main/java/com/example/mealgo/MealGoApplication.kt
@@ -7,7 +7,7 @@ import dagger.hilt.android.HiltAndroidApp
 class MealGoApplication: Application()
 
 /** 추가할 기능들
- 1. 학교 검색에 Flow로 Instant Search 구현
+ 1. 학교 검색에 Flow로 Instant Search 구현 - 완료
  2. Room에 급식 데이터 저장
  3. Room 기반 페이징으로 스와이프 해서 급식 넘기기
  4. 기상 시각에 맞춰 푸시 알림으로 급식 전송
