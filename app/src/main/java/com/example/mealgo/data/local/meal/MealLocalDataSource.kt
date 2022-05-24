c7eee6ff1157c5d2ad95f644e70cd248f1adac2d
양태웅 <didxodnd0422@gmail.com>
2022-05-24 17:54:09 +0900

    Implement MealLocalDataSource

--- app/src/main/java/com/example/mealgo/data/local/meal/MealLocalDataSource.kt
+++ app/src/main/java/com/example/mealgo/data/local/meal/MealLocalDataSource.kt
@@ -1,10 +1,16 @@
 package com.example.mealgo.data.local.meal
 
+import com.example.mealgo.data.local.MealDao
+import com.example.mealgo.data.local.meal.model.MealEntity
 import javax.inject.Inject
 
 
 class MealLocalDataSource @Inject constructor(
-
+    private val mealDao: MealDao
 ) {
+    suspend fun insertMeal(mealEntity: MealEntity) = mealDao.insertMeal(mealEntity)
 
+    suspend fun deleteMeal(mealEntity: MealEntity) = mealDao.deleteMeal(mealEntity)
+
+    fun getMeal(mealDate: String) = mealDao.getMeal(mealDate)
 }
