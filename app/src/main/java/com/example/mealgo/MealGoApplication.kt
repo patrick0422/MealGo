16724a1084383d227f7220e283b32e55b60d712c
양태웅 <didxodnd0422@gmail.com>
2022-05-26 00:01:45 +0900

    Update milestone status

--- app/src/main/java/com/example/mealgo/MealGoApplication.kt
+++ app/src/main/java/com/example/mealgo/MealGoApplication.kt
@@ -8,7 +8,7 @@ class MealGoApplication: Application()
 
 /** 추가할 기능들
  1. 학교 검색에 Flow로 Instant Search 구현 - 완료
- 2. Room에 급식 데이터 저장
+ 2. Room에 급식 데이터 저장 - 완료
  3. Room 기반 페이징으로 스와이프 해서 급식 넘기기
  4. 기상 시각에 맞춰 푸시 알림으로 급식 전송
  */
