0c3ab67c30873936f8333b00bc5384ecc9fb868c
양태웅 <didxodnd0422@gmail.com>
2022-05-25 23:45:20 +0900

    Add schoolName property in MealEntity

--- app/src/main/java/com/example/mealgo/data/local/meal/model/MealEntity.kt
+++ app/src/main/java/com/example/mealgo/data/local/meal/model/MealEntity.kt
@@ -3,10 +3,10 @@ package com.example.mealgo.data.local.meal.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(primaryKeys = ["mealDate", "schoolName"])
 data class MealEntity(
-    @PrimaryKey(autoGenerate = false)
     val mealDate: String,
+    val schoolName: String,
     val breakFast: String,
     val lunch: String,
     val dinner: String
